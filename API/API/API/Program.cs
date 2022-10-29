31a81eac153cdd89bcbfff70e659c1aa6bf41939
Mayis-Mazin <mayis.mazin@gmail.com>
2022-10-30 01:06:40 +0200

    (update): make The API Public

--- API/API/API/Program.cs
+++ API/API/API/Program.cs
@@ -11,16 +11,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
  builder.Services.AddDbContext<userFormContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("GNContext")));
+
+//alow every request and making our api public
+ builder.Services.AddCors(options => options.AddPolicy("defult", policy => {
+    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+//use the injected cors in the pipeline
+app.UseCors("defult");
 
 app.UseAuthorization();
 
